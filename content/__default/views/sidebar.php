21b6a9554dc74f57891912cbd6cc556c2ee7ed63
zig <jasekz1@gmail.com>
2015-02-09 22:37:09 +0000

    Change example home page download button to point to the Github repo.

--- content/__default/views/sidebar.php
+++ content/__default/views/sidebar.php
@@ -1,4 +1,4 @@
 <?php if(!defined('SYSTEM')) die('no access');  ?>
-<a href="http://sourceforge.net/projects/simpleapp-php/" target="_blank" style="margin:70px 0 0 0;padding:0px;">
+<a href="https://github.com/jasekz/simple-app-for-php" target="_blank" style="margin:70px 0 0 0;padding:0px;">
 <img  src="<?php echo TEMPLATE_PATH; ?>/images/download-now.gif" width="200" />
 </a>
