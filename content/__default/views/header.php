caeeaa56f4854937b88c803293c0104ad3427ca9
zig <jasekz1@gmail.com>
2015-02-09 23:15:52 +0000

    Change a url to point to Github instead of SourceForge; fix logo img path.

--- content/__default/views/header.php
+++ content/__default/views/header.php
@@ -3,6 +3,6 @@
 <ul id="main-menu">
 	<li><a href="<?php echo page('admin'); ?>" target="_blank" >Admin</a></li>
     <li><a href="http://simpleapp.info/documentation.html" target="_blank" >Documentation</a></li>
-    <li><a href="http://sourceforge.net/projects/simpleapp-php/" target="_blank" >Downloads</a></li>
+    <li><a href="https://github.com/jasekz/simple-app-for-php" target="_blank" >Downloads</a></li>
     <li><a href="<?php echo page(''); ?>" <?php if($this->page == 'home' || $this->page == '__default') echo 'class="selected"'; ?>>Home</a></li>    
 </ul>
