a700c3ca0a1e2e77c0d1791247748cdafda02e95
Zig <jasekz1@gmail.com>
2015-04-15 09:08:33 -0500

    Changed copyright dates in footer.

--- content/__default/views/footer.php
+++ content/__default/views/footer.php
@@ -1,3 +1,3 @@
 <?php if(!defined('SYSTEM')) die('no access');  ?>
 
-<p>Copyright (c) 2012 SimpleApp open source application development framework for PHP 5</p>
+<p>Copyright (c) 2012 - <?php echo date('Y'); ?> SimpleApp open source application development framework for PHP 5</p>
